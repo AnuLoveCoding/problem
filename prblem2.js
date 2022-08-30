3508b9487dedf7761064d7628a2db0c35560935d
anuLoveCoading <anusrivas07@gmail.com>
2022-08-30 18:25:37 +0530

    New! project

--- prblem2.js
+++ prblem2.js
@@ -1,1 +1,26 @@
 console.log("Anurag welcome! World")
+
+
+
+// Input:
+// N = 6
+// A[] = {3, 2, 1, 56, 10000, 167}
+// Output:
+// min = 1, max =  10000
+
+let arr=[3,2,1,56,10000,167]
+let note =Infinity
+
+for(let i=0;i<=6;i++){
+    if(arr[i]<note)
+    note=arr[i]
+}
+console.log(note)
+
+
+let note1= -Infinity
+for(let j=0;j<=6;j++){
+   if(arr[j]>note1)
+   note1=arr[j]
+}
+console.log(note1)
