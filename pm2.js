36a6efa84a70f7536306ae3bfb96585bfd6eb062
anuLoveCoading <anusrivas07@gmail.com>
2022-08-31 18:15:28 +0530

    pm2

---
+++ pm2.js
@@ -0,0 +1,34 @@
+console.log("Anurag welcome! World")
+
+
+
+// Input:
+// N = 6
+// A[] = {3, 2, 1, 56, 10000, 167}
+// Output:
+// min = 1, max =  10000
+
+let arr=[3,2,1,56,10000,167]
+let note =Infinity
+
+for(let i=0;i<=6;i++){
+    if(arr[i]<note)
+    note=arr[i]
+}
+console.log(note)
+
+
+// let note1= -Infinity
+// for(let j=0;j<=6;j++){
+//    if(arr[j]>note1)
+//    note1=arr[j]
+// }
+// console.log(note1)
+
+for( let i=1;i<=10;i++){
+    let bag = ""
+     for(let j=1;j<=10;j++){
+        bag+="6"+" "
+    }console.log(bag)
+}
+
--- wl.js
+++ wl.js
@@ -35,10 +35,18 @@ while(l<=180){
 
 
 const d= new Date("2022-03-31")
-// document.getElementById("demo")
+
 console.log(d)
 
 
 
 const f = new Date();
 console.log(f)
+
+
+
+const date1 = new Date('December 17, 1995 03:24:00')
+onsole.log(date1)
+
+
+
